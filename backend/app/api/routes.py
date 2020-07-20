703666cb131a2a67a5df7867bde6ec7b41c16cdf
Rafael A. Bizao <rabizao@gmail.com>
2020-07-20 17:35:56 -0300

    submit nested objects

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,19 +1,26 @@
 from . import bp
 from flask.views import MethodView
+from app import db
 from app.schemas import JobQueryArgsSchema, JobSchema
+from app.models import Job
 
 
-@bp.route('/teste')
+@bp.route('/jobs')
 class Jobs(MethodView):
-    @bp.arguments(JobQueryArgsSchema, location='query')
+    @bp.arguments(JobQueryArgsSchema, location="query")
     @bp.response(JobSchema(many=True))
-    def get(self, args):
-        return "Hello apiii"
+    @bp.paginate()
+    def get(self, args, pagination_parameters):
+        """List all jobs currently available"""
+        data, total = Job.get(args, pagination_parameters.page,
+                              pagination_parameters.page_size)
+        pagination_parameters.item_count = total
+        return data
 
-
-@bp.route('/teste22')
-class Jobs2(MethodView):
-    @bp.arguments(JobQueryArgsSchema, location='query')
-    @bp.response(JobSchema(many=True))
-    def get(self, args):
-        return "Hello apiii"
+    @bp.arguments(JobSchema)
+    @bp.response(JobSchema)
+    def post(self, job):
+        """Post a new job with tags"""
+        db.session.add(job)
+        db.session.commit()
+        return job
